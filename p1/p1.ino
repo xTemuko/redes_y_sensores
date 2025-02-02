c0b4b0691d1596bf4461ac054ae69e2ada24bd01
Manuel <manruiare@gmail.com>
2025-02-02 18:17:33 +0100

    Working on interruptions timers

--- p1/p1.ino
+++ p1/p1.ino
@@ -1,18 +1,27 @@
+#include <Arduino.h>
+#include "BBTimer.hpp"  
+
 #define ADC_PIN A6  
 #define VREF 3.3    
 
-void setup() {
-    Serial.begin(115200);  
-}
+BBTimer my_timer(BB_TIMER0);  
 
-void loop() {
-
-    int adcValue = analogRead(ADC_PIN); 
-    float voltage = (adcValue * VREF) / 1023.0;  
+void timerCallback() {
+    int adcValue = analogRead(ADC_PIN);
+    float voltage = (adcValue * VREF) / 1023.0; 
 
     char buffer[50];
     sprintf(buffer, "ADC: %d | Voltaje: %.2f V", adcValue, voltage);
-    Serial.println(buffer);  
+    Serial.println(buffer);
+}
 
-    delay(1000);  
+void setup() {
+    Serial.begin(115200);
+    my_timer.setupTimer(5000000, timerCallback); 
+    my_timer.timerStart();
+}
+
+void loop() {
+    Serial.println("IDLE Task");
+    delay(1000);
 }
