abe404d4e98c28e4f1b961af012273a5dfbe8408
Xae <manruiare@gmail.com>
2025-02-05 22:01:50 +0100

    1.2 Modify for easy interrupt inside loop

--- p1/p1.ino
+++ p1/p1.ino
@@ -4,24 +4,35 @@
 #define ADC_PIN A6  
 #define VREF 3.3    
 
-BBTimer my_timer(BB_TIMER0);  
+BBTimer my_timer(BB_TIMER4);
+
+volatile bool flag = false;
+volatile int adcValue = 0;
 
 void timerCallback() {
-    int adcValue = analogRead(ADC_PIN);
-    float voltage = (adcValue * VREF) / 1023.0; 
-
-    char buffer[50];
-    sprintf(buffer, "ADC: %d | Voltaje: %.2f V", adcValue, voltage);
-    Serial.println(buffer);
+  flag = true;
 }
 
 void setup() {
     Serial.begin(115200);
+
+    Serial.println("Iniciando temporizador...");
     my_timer.setupTimer(5000000, timerCallback); 
     my_timer.timerStart();
 }
 
 void loop() {
+
     Serial.println("IDLE Task");
+    if (flag) {  // Si la interrupción se activó
+        flag = false;  // Resetear flag
+        adcValue = analogRead(ADC_PIN);  // Leer ADC en el loop()
+        
+        float voltage = (adcValue * VREF) / 1023.0; 
+
+        char buffer[50];
+        sprintf(buffer, "ADC: %d | Voltaje: %.2f V", adcValue, voltage);
+        Serial.println(buffer);
+    }
     delay(1000);
 }
