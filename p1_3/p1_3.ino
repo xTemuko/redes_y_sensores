63bd32b192a3141cf809bdc33c38a05bcd6ab80e
Xae <manruiare@gmail.com>
2025-02-08 12:29:29 +0100

    1.3 output using PWM - done

--- p1_3/p1_3.ino
+++ p1_3/p1_3.ino
@@ -1,7 +1,8 @@
 #include <Arduino.h>
-#include "BBTimer.hpp"  
+#include "BBTimer.hpp"
 
-#define ADC_PIN A6  
+#define ADC_PIN A0
+#define PWM_PIN D2
 #define VREF 3.3    
 
 BBTimer my_timer(BB_TIMER4);
@@ -15,6 +16,7 @@ void timerCallback() {
 
 void setup() {
     Serial.begin(115200);
+    pinMode(PWM_PIN, OUTPUT);
 
     Serial.println("Iniciando temporizador...");
     my_timer.setupTimer(5000000, timerCallback); 
@@ -22,17 +24,21 @@ void setup() {
 }
 
 void loop() {
-
     Serial.println("IDLE Task");
-    if (flag) {  // Si la interrupción se activó
-        flag = false;  // Resetear flag
-        adcValue = analogRead(ADC_PIN);  // Leer ADC en el loop()
-        
+    
+    if (flag) {  
+        flag = false;  
+        adcValue = analogRead(ADC_PIN);        
         float voltage = (adcValue * VREF) / 1023.0; 
 
+    
+        int dutyCycle = map(adcValue, 0, 1023, 0, 255);
+        analogWrite(PWM_PIN, dutyCycle);
+
         char buffer[50];
-        sprintf(buffer, "ADC: %d | Voltaje: %.2f V", adcValue, voltage);
+        sprintf(buffer, "ADC: %d | Voltaje: %.2f V | PWM: %d", adcValue, voltage, dutyCycle);
         Serial.println(buffer);
     }
+
     delay(1000);
 }
